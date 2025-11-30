f708611208659ea09848d1c8cb8ed9dfa698ef1c
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-01 02:07:35 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://ai-finance-tracker-9l91.onrender.com',
 });
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
