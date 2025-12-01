89314669fe35205d95516932d89cdecc9c499647
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-02 02:54:16 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
-console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
-
-axios.defaults.withCredentials = true;
-
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
 });
 
+// Attach token automatically
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
