2eb0e2db6e5205afa4aabd96ff6f0fc85cfa90da
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-01 21:16:24 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
 import Dashboard from './components/Dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 export default function App() {
-  const token = localStorage.getItem('token');
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ token ? <Dashboard /> : <Navigate to="/login" /> } />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
