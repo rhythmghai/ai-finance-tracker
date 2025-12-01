0b6687aaf4e9ac316063f3eab2a684cdcfb08a64
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-02 02:51:25 +0530

    Update Bill.js

--- server/models/Bill.js
+++ server/models/Bill.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const BillSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  name: String,
-  amount: Number,
-  dueDate: Date,
-  recurring: { type: Boolean, default: true }
-});
+const BillSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    name: String,
+    amount: Number
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Bill', BillSchema);
+module.exports = mongoose.model("Bill", BillSchema);
