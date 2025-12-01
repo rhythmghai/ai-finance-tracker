8a4d7f3f4ae121e0b1a038f8f56aff9f8b6ad2a5
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-02 00:52:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -35,7 +35,11 @@ const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    const PORT = process.env.PORT || 10000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+});
+
   })
   .catch(err => {
     console.error('MongoDB connection error:', err);
