c21e75b7e41f4934119814fc124909004e820999
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-02 00:49:07 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-finance-tracker-9l91.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((config) => {
