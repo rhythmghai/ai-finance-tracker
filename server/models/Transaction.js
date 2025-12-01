1be55a2503ba069fbb52e446cd76a3971226ca82
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-02 02:51:04 +0530

    Update Transaction.js

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const TransactionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  amount: Number,
-  category: String,
-  note: String,
-  type: { type: String, enum: ['income','expense'], default: 'expense' },
-  date: { type: Date, default: Date.now }
-});
+const TransactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    amount: Number,
+    category: String,
+    note: String,
+    type: { type: String } // "expense" or "income"
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Transaction', TransactionSchema);
+module.exports = mongoose.model("Transaction", TransactionSchema);
