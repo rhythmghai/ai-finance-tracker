430f41865d3f2e475f82ebc82375620fbbbb0005
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-01 21:49:36 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
-const bcrypt = require("bcryptjs");
-
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  passwordHash: String,
+  name: { type: String, default: "" },
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
   monthlyIncome: { type: Number, default: 0 },
   savingsGoal: { type: Number, default: 0 }
 });
 
-UserSchema.methods.setPassword = async function(pw) {
-  this.passwordHash = await bcrypt.hash(pw, 10);
+// Set password
+UserSchema.methods.setPassword = async function(password) {
+  const salt = await bcrypt.genSalt(10);
+  this.passwordHash = await bcrypt.hash(password, salt);
 };
 
-UserSchema.methods.validatePassword = async function(pw) {
-  return await bcrypt.compare(pw, this.passwordHash);
+// Validate password
+UserSchema.methods.validatePassword = async function(password) {
+  return await bcrypt.compare(password, this.passwordHash);
 };
 
 module.exports = mongoose.model('User', UserSchema);
