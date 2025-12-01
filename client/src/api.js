efc0744c1344549cb9df72967f99cfc2e4e7167d
rhythmghai <rrhythm1_be23@thapar.edu>
2025-12-02 01:00:46 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,9 +2,11 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
+
+
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
